835f83b371eb08bda77f566b5952a92a97b9fe6c
AnmolSaraswa <113250050+AnmolSaraswa@users.noreply.github.com>
2023-02-15 06:56:23 -0800

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,52 @@
+ var y = Math.floor(Math.random()*10+1);
+
+ playername= localStorage.getItem("player_name");
+
+ var guess = 1;
+
+ var x  = document.getElementById("guessField").value;
+
+ function submit()
+{
+
+   localStorage.setItem("number", x);
+
+   document.getElementById("guessField").innerHTML="";
+
+}
+ if (x==y)
+ {
+
+    alert("CONGRATULATIONS!!!"+playername+" YOU GUESSED IT RIGHT IN"+guess+"GUESS");
+
+    guess=1;
+
+ }
+
+ else if(x>y)
+ {
+
+    guess++;
+
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+
+ }
+
+ else 
+ {
+    
+    guess++;
+
+    alert("OOPS SORRY!! TRY A GREATER NUMBER");
+
+ }
+
+
+ function playAgain()
+ {
+
+    y = Math.floor(Math.random()*10+1);
+
+    document.getElementById("guessField").innerHTML="";
+ }
+
